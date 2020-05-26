f7b7b48520059909654c30ca741b30ba9fd0950e
Shweta <shwetasha03@gmail.com>
2020-05-26 23:31:30 +0530

    First commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from views.items import item_blueprint
+
+app = Flask(__name__)
+app.register_blueprint(item_blueprint, url_prefix="/items")
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+#url = "https://www.johnlewis.com/2018-apple-ipad-pro-12-9-inch-a12x-bionic-ios-wi-fi-512gb/space-grey/p3834538"
+#tag_name = "p"
+#query = {"class": "price price--large"}
+
+#ipad = Item(url, tag_name, query)
+#ipad.save_to_mongo()
+
+#items_loaded = Item.all()
+#print(items_loaded)
+#print(items_loaded[0].load_price())
